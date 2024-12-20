0b90dda63c1174e0e9db1cd0b2e7addf144b6dd3
Viki <hi@viki.moe>
2024-12-20 09:54:46 +0800

    chore: save

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,54 @@
 fn main() {
-  println!("Hello, world!");
+    let mut target = 1232345;
 
-  let mut a = 1;
-  let b = 2;
-  a = a + b;
+    while target > 17 {
+        target = target % 17
+    }
 
-  println!("a = {}, {}", a, 2);
+    println!("The final value is: {}", target);
 
-  println!(
-      "i32 {}, i8 {}, usize {}, u8 {}, f32 {}, f64 {}, bool {}, char {}",
-      std::mem::size_of::<i32>(),
-      std::mem::size_of::<i8>(),
-      std::mem::size_of::<usize>(),
-      std::mem::size_of::<u8>(),
-      std::mem::size_of::<f32>(),
-      std::mem::size_of::<f64>(),
-      std::mem::size_of::<bool>(),
-      std::mem::size_of::<char>()
-  );
+    for n in 1..=5 {
+        println!("n: {}", n);
+    }
+
+    for e in [1, 2, 3] {
+        println!("e: {}", e);
+    }
+
+    let mut num = 0;
+
+    let x = loop {
+        num += 1;
+        if num == 10 {
+            break num * 2;
+        }
+    };
+
+    println!("The value of x is: {}", x);
+
+    let a = 10;
+    println!("before: {a}");
+
+    let str = format!("a is {}", a);
+    println!("{}", str);
+    
+
+    {
+        let a = "hello";
+        println!("inner scope: {a}");
+
+        let a = true;
+        println!("shadowed in inner scope: {a}");
+
+        let a = 10;
+        println!("shadowed again in inner scope: {a}");
+    }
+
+    dbg!(a);
+
+    println!("after: {a}");
+
+    // unreachable!("This code should never be reached");
+
+    // todo!("Implement the rest of the program");
 }
